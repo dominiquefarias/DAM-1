53c66fca19bf040511f3d3a4603663e116fbb928
Dominique Farias <dominiquefarias21@gmail.com>
2025-11-19 09:51:17 +0100

    19/11

---
+++ programacion/006-Aplicaciones de las estructuras de almacenamiento/003-Matrices multidimensionales/018-cambio de tipo de dato.py
@@ -0,0 +1,10 @@
+tupla = ("manzanas","peras","platanos")
+# Ncesito meter una fruta mas 
+print(tupla)
+lista = list(tupla) #  convierto una tupla en una lista
+print(lista)
+lista.append("fresas")
+
+# Ahora supongamos que tengo que volver a tupla
+nueva_tupla = tupla(lista)
+print(nueva_tupla)
---
+++ programacion/006-Aplicaciones de las estructuras de almacenamiento/003-Matrices multidimensionales/019-parto.py
@@ -0,0 +1,27 @@
+import pickle
+
+agenda = []
+
+while True:
+
+	print("Selecciona una opción.")
+	print("1. Insertar un registro")
+	print("2. Leer registros")
+	print("3. Guardar archivos")
+	
+	if opcion == 1:
+		# Insertar
+		nombre = input("Dime tu nombre: ")
+		apellidos = input("Dime tus apellidos: ")
+		email = input("Dime tu email: ")
+		telefono = input("Dime tu telefono")
+		agenda.append([nombre,apellidos,email,telefono])
+	elif opcion == 2:
+		# Imprimir
+		print(agenda)
+	elif opcion == 3:
+		# Guardar
+		archivo = open("agenda.bin","wb")
+		pickle.dump(agenda,archivo)
+		archivo.close
+	
