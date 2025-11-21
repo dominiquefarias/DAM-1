c7d0c3ea79d0f55fe030718c4d8c24efd884d62c
Dominique Farias <dominiquefarias21@gmail.com>
2025-11-21 08:44:32 +0100

    21-11

---
+++ Proyecto intermodular/jinja/002-multipagina.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/inicio")
+def inicio():
+    return render_template("inicio.html")
+	
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+@app.route("/sobremi")
+def inicio():
+    return render_template("sobremi.html")
+	
+if __name__ == "__main__":
+    app.run(debug=True)
+
+@app.route("/contacto")
+def inicio():
+    return render_template("contacto.html")
+	
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
