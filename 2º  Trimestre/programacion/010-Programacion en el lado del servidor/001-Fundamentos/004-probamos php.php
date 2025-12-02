0fac965f0e40faf6910f31d60bf7b03caeb34b13
Dominique Farias <dominiquefarias21@gmail.com>
2025-12-02 11:14:23 +0100

    .

---
+++ 2º  Trimestre/programacion/010-Programacion en el lado del servidor/001-Fundamentos/004-probamos php.php
@@ -0,0 +1,7 @@
+Esto no es PHP
+
+<?php
+	echo "Esto si es php";
+?>
+
+Esto ya no es php
---
+++ 2º  Trimestre/programacion/010-Programacion en el lado del servidor/001-Fundamentos/005-comentarios en php.php
@@ -0,0 +1,9 @@
+<?php
+	echo "Hola mundo en PHP";
+	// "echo" en php es como un print en python
+	// Esto es un comentario de una linea 
+	/*
+		Esto es una linea de comentario
+		Esto tambien es una linea de comentario
+	*/
+?>
