93e56a727cc58fbf47f3e13f5d2b1b41f2fac63f
Dominique Farias <dominiquefarias21@gmail.com>
2025-12-09 13:32:58 +0100

    .

--- 2º  Trimestre/Proyecto intermodular/plantilla proyecto/aplicacion/index.php
+++ 2º  Trimestre/Proyecto intermodular/plantilla proyecto/aplicacion/index.php
@@ -2,37 +2,18 @@
 <html lang="es">
 
 <head>
-    <title>Domasa - Noticias tecnológicas</title>
+    <title>El jocarsa - Noticias tecnológicas</title>
     <meta charset="utf-8">
     <link rel="stylesheet" href="css/estilo.css">
 </head>
 
 <body>
     <header>
-        <h1>Domasa</h1>
+        <h1>El jocarsa</h1>
         <h2>Noticias tecnológicas</h2>
     </header>
     <main>
-        <?php
-        $host = "localhost";
-        $user = "periodico";
-        $pass = "Periodico123$";
-        $db = "periodico";
-        $conexion = new mysqli($host, $user, $pass, $db);
-        $sql = "SELECT * FROM noticias";
-        $resultado = $conexion->query($sql);
-        while ($fila = $resultado->fetch_assoc()) {
-            echo '
-            <article>
-              <h3>' . $fila['titulo'] . '</h3>
-              <time>' . $fila['fecha_publicacion'] . '</time>
-              <p>' . $fila['autor_id'] . '</p>
-              <p>' . $fila['contenido'] . '</p>
-            </article>
-          ';
-        }
-        $conexion->close();
-        ?>
+        <?php include "inc/listar_articulos.php"; ?>
     </main>
     <footer>
     </footer>
