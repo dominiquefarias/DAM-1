d1b281f3ea6b27f2a9d1c71b6e34fbd6c1643375
Dominique Farias <dominiquefarias21@gmail.com>
2025-12-10 10:25:10 +0100

    .

--- 2º  Trimestre/Proyecto intermodular/plantilla proyecto/aplicacion/inc/listar_articulos.php
+++ 2º  Trimestre/Proyecto intermodular/plantilla proyecto/aplicacion/inc/listar_articulos.php
@@ -7,7 +7,7 @@ $conexion = new mysqli($host, $user, $pass, $db);
 $sql = "SELECT * FROM noticias";
 $resultado = $conexion->query($sql);
 while ($fila = $resultado->fetch_assoc()) {
-    echo '
+  echo '
             <article>
               <h3>' . $fila['titulo'] . '</h3>
               <time>' . $fila['fecha_publicacion'] . '</time>
