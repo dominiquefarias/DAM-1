e9e61fac21dccacafb0f53d8aeedb9b1daeb85fb
Dominique Farias <dominiquefarias21@gmail.com>
2025-12-10 13:20:07 +0100

    .

---
+++ 2º  Trimestre/Base de datos/008-Proyectos/002-CRUD/001-create/001-crear.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>Jose Vicente Carratala</title>
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <form action="procesa.php" method="POST">
+        <input type="text" name="nombre" placeholder="nombre">
+        <input type="text" name="puesto" placeholder="puesto">
+        <input type="text" name="salario" placeholder="salario">
+        <input type="text" name="fecha_contratacion" placeholder="fecha_contratacion">
+        <input type="text" name="departamento" placeholder="departamento">
+        <input type="submit">
+    </form>
+</body>
+
+</html>
