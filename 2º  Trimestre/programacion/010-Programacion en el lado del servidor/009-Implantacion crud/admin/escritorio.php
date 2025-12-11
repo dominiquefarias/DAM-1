db4ea5d2accc8daf12c8328acd49cb0491803307
Dominique Farias <dominiquefarias21@gmail.com>
2025-12-11 12:09:07 +0100

    .

---
+++ 2º  Trimestre/programacion/010-Programacion en el lado del servidor/009-Implantacion crud/admin/escritorio.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>Domasa - Panel de control</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <nav>
+        <button>Noticias</button>
+        <button>Autores</button>
+    </nav>
+    <main>
+        <?php
+        if (isset($_GET['accion'])) {								// Si hay "accion" en la URL
+            if ($_GET['accion'] == "nuevo") {					// Si la acción es "nuevo"
+                include "inc/create/formulario.php";	// En ese caso mete el formulario
+            }
+        } else {																		// En caso contrario
+            include "inc/read/leer.php"; 						// Enseñame la tabla
+        }
+        ?>
+        <a href="?accion=nuevo" id="nuevo">+</a>
+    </main>
+</body>
+
+</html>
---
+++ 2º  Trimestre/programacion/010-Programacion en el lado del servidor/009-Implantacion crud/index.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+    <title>Domasa - Noticias tecnológicas</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/estilo.css">
+</head>
+
+<body>
+    <header>
+        <h1>Domasa</h1>
+        <h2>Noticias tecnológicas</h2>
+    </header>
+    <main>
+        <?php include "inc/listar_articulos.php"; ?>
+    </main>
+    <footer>
+    </footer>
+</body>
+
+</html>
