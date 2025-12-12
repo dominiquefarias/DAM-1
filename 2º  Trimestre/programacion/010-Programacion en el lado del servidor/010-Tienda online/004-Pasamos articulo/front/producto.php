b288fb57333f766a49c20db2490b0ca1864f08b3
Dominique Farias <dominiquefarias21@gmail.com>
2025-12-12 14:07:07 +0100

    .

--- 2º  Trimestre/programacion/010-Programacion en el lado del servidor/010-Tienda online/004-Pasamos articulo/front/producto.php
+++ 2º  Trimestre/programacion/010-Programacion en el lado del servidor/010-Tienda online/004-Pasamos articulo/front/producto.php
@@ -1,6 +1,6 @@
 <?php include "inc/cabecera.php"; ?>
 
-<section id="catalogo">
+<section id="paginaproducto">
 
     <?php
     $host = "localhost";
@@ -17,10 +17,16 @@
         ?>
         <article>
             <div class="imagen"></div>
-            <h3><?= $fila['nombre_producto'] ?></h3>
             <p><?= $fila['precio'] ?></p>
+            <form action="carrito.php" method="POST">
+                <input type="hidden" name="id" value="<?= $fila['id'] ?>">
+                <input type="number" min=1 max=10 value=1>
+                <input type="submit" value="Comprar">
+            </form>
+        </article>
+        <article>
+            <h3><?= $fila['nombre_producto'] ?></h3>
             <p><?= $fila['descripcion'] ?></p>
-            <a href="producto.php?id=<?= $fila['id'] ?>">Comprar</a> <!-- NUEVO -->
         </article>
         <?php
     }
@@ -31,23 +37,23 @@
 </section>
 
 <style>
-    #catalogo {
-        display: grid;
-        grid-template-columns: repeat(3, 1fr);
+    #paginaproducto {
+        display: flex;
         gap: 20px;
     }
 
-    #catalogo article {
+    #paginaproducto article {
         text-align: center;
+        flex: 1;
     }
 
-    #catalogo article .imagen {
+    #paginaproducto article .imagen {
         background: darkorchid;
         height: 100px;
         border-radius: 5px 5px 0px 0px;
     }
 
-    #catalogo article a {
+    #paginaproducto article a {
         background: darkorchid;
         padding: 10px;
         border-radius: 5px;
