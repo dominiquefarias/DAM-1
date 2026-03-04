90b50bf5659ffab28335bb50694097619deb586f
= <dominiquefarias21@gmail.com>
2026-03-04 13:51:09 +0100

    .

---
+++ 2º y 3º Trimestre/programacion/015-C y cpp/001-C/010-comparacion.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int menorque = 4<3;
+	printf("%i",menorque);
+  int mayorque = 4>3;
+	printf("%i",mayorque);
+  int menoroigual = 4<=3;
+	printf("%i",menoroigual);
+  int mayorogigual = 4>=3;
+	printf("%i",mayorque);
+  return 1;
+}
---
+++ 2º y 3º Trimestre/programacion/015-C y cpp/001-C/021-creo mi propia funcion.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int calculoDoble(int numero){
+	int doble = numero*2;
+  return doble;
+}
+
+int main(){
+	int edad = 47;
+  
+  return 0;
+}
